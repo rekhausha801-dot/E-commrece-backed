5538913fb38d0ece55fdd364ecb95836cc36b313
Relietech <relietech2023@gmail.com>
2026-08-19 16:12:04 +0530

    login

--- models/User.js
+++ models/User.js
@@ -36,7 +36,7 @@ userSchema.pre("save", async function () {
   this.password = await bcrypt.hash(this.password, salt);
 });
 
-// Match user entered password to hashed password in database
+
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
